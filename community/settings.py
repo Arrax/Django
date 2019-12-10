38e321d8c9b562ddfc56d9d33c22e5ff2d5cb93f
mightusa <xiaogouwoainia@163.com>
2019-12-10 22:50:31 +0800

    用户认证模块完成“

--- community/settings.py
+++ community/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'authentication',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -134,3 +137,11 @@ STATICFILES_DIRS = [
 # custom user model
 
 AUTH_USER_MODEL = 'authentication.User'
+
+# django.contrib.auth.urls
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
